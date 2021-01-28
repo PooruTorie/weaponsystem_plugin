21aaf1ffaa1f27001fe3e435ebfd40dccacaf7b6
BuildTools <unconfigured@null.spigotmc.org>
2021-01-28 15:42:54 +0100

    U

--- src/de/paul/weaponsystem/config/WeaponConfig.java
+++ src/de/paul/weaponsystem/config/WeaponConfig.java
@@ -2,13 +2,23 @@ package de.paul.weaponsystem.config;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.function.Consumer;
 
+import org.json.simple.JSONArray;
 import org.json.simple.parser.ParseException;
+
+import de.paul.weaponsystem.weapon.Weapon;
+import de.paul.weaponsystem.weapon.Weapon.WeaponType;
 
 public class WeaponConfig extends Config {
 
+	private WeaponType type;
 	private String name;
 	private String itemName;
+	private int itemID;
+	private ArrayList<String> itemLore = new ArrayList<>();
+	private int meleeDamage;
 
 	public WeaponConfig(File f) throws IOException, ParseException {
 		super(f);
@@ -17,8 +27,31 @@ public class WeaponConfig extends Config {
 	}
 	
 	private void load() {
+		type = Weapon.WeaponType.valueOf((String) get("type"));
 		name = (String) get("name");
 		itemName = getChatColorString("item_name");
+		itemID = ((Long) get("item_id")).intValue();
+		((JSONArray) get("item_lore")).forEach(new Consumer<Object>() {
+			@Override
+			public void accept(Object t) {
+				itemLore.add((String) t);
+			}});
+		switch (type) {
+		case Gun:
+			gunDamage = ((Long) get("damage")).intValue();
+			break;
+		case Melee:
+			meleeDamage = ((Long) get("damage")).intValue();
+			break;
+		}
+	}
+	
+	public Weapon toWeapon() {
+		return new Weapon(this);
+	}
+	
+	public WeaponType getType() {
+		return type;
 	}
 	
 	public String getName() {
@@ -27,5 +60,16 @@ public class WeaponConfig extends Config {
 	
 	public String getItemName() {
 		return itemName;
+	}
+	
+	public int getItemID() {
+		return itemID;
+	}
+	public ArrayList<String> getItemLore() {
+		return itemLore;
+	}
+	
+	public int getMeleeDamage() {
+		return meleeDamage;
 	}
 }
---
+++ src/de/paul/weaponsystem/weapon/Weapon.java
@@ -0,0 +1,15 @@
+package de.paul.weaponsystem.weapon;
+
+import de.paul.weaponsystem.config.WeaponConfig;
+
+public class Weapon {
+	
+	
+	public Weapon(WeaponConfig config) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public enum WeaponType {
+		Gun, Melee;
+	}
+}
