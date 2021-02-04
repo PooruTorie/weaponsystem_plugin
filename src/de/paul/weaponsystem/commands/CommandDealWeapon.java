66963c6086ec6b8e934152836ffe9aa3c5dbbd53
BuildTools <unconfigured@null.spigotmc.org>
2021-02-04 18:23:15 +0100

    u

--- src/de/paul/weaponsystem/commands/CommandDealWeapon.java
+++ src/de/paul/weaponsystem/commands/CommandDealWeapon.java
@@ -6,12 +6,22 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+import de.paul.weaponsystem.WeaponSystem;
 
 public class CommandDealWeapon implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		// TODO Auto-generated method stub
+		if (sender instanceof Player) {
+			Player p = (Player) sender;
+			if (p.hasPermission((String) WeaponSystem.loadConfig("config", "permissions").get("dealweapon"))) {
+				
+			} else {
+				p.sendMessage(WeaponSystem.prefix+WeaponSystem.loadConfig("config", "messages").getChatColorString("nopermission"));
+			}
+		}
 		return false;
 	}
 
